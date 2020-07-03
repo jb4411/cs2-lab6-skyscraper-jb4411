ffad0668d6bfa36168c1c744f84515c0091c7679
Jesse Burdick-Pless <jb4411@rit.edu>
2020-07-02 23:04:05 -0400

    Worked on isValid

--- src/SkyscraperConfig.java
+++ src/SkyscraperConfig.java
@@ -20,6 +20,9 @@ public class SkyscraperConfig implements Configuration {
     private int[][] board;
     private int row;
     private int col;
+    private ArrayList<HashSet<Integer>> rows;
+    private ArrayList<HashSet<Integer>> columns;
+    private Boolean preFilled = false;
 
     /**
      * Constructor
@@ -43,8 +46,14 @@ public class SkyscraperConfig implements Configuration {
 
         this.DIM = f.nextInt();
 
-        this.lookingValues = new int[this.DIM][this.DIM];
+        this.lookingValues = new int[4][this.DIM];
         this.board = new int[this.DIM][this.DIM];
+        this.rows = new ArrayList<>();
+        this.columns = new ArrayList<>();
+        for (int i = 0; i < this.DIM; i++) {
+            this.rows.add(i, new HashSet<>());
+            this.columns.add(i, new HashSet<>());
+        }
 
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < this.DIM; j++) {
@@ -54,12 +63,15 @@ public class SkyscraperConfig implements Configuration {
 
         for (int i = 0; i < this.DIM; i++) {
             for (int j = 0; j < this.DIM; j++) {
-                this.board[i][j] = f.nextInt();
+                int num = f.nextInt();
+                this.board[i][j] = num;
+                this.rows.get(i).add(num);
+                this.columns.get(j).add(num);
             }
         }
 
         this.row = 0;
-        this.col = 0;
+        this.col = -1;
         // close the input file
         f.close();
     }
@@ -69,23 +81,144 @@ public class SkyscraperConfig implements Configuration {
      *
      * @param copy SkyscraperConfig instance
      */
-    public SkyscraperConfig(SkyscraperConfig copy) {
+    public SkyscraperConfig(SkyscraperConfig copy, int row, int col, int num) {
         this.DIM = copy.DIM;
-        this.row = copy.row;
-        this.col = copy.col;
+        this.row = row;
+        this.col = col;
+        this.lookingValues = new int[4][this.DIM];
         this.board = new int[copy.DIM][copy.DIM];
+        this.rows = new ArrayList<>();
+        this.columns = new ArrayList<>();
+
+        for (int i = 0; i < this.DIM; i++) {
+            this.rows.add(i,new HashSet<>(copy.rows.get(i)));
+            this.columns.add(i,new HashSet<>(copy.columns.get(i)));
+        }
 
         for (int r=0; r<this.DIM; r++) {
             System.arraycopy(copy.board[r], 0, this.board[r], 0, this.DIM);
+            System.arraycopy(copy.lookingValues[r], 0, this.lookingValues[r], 0, 4);
         }
+        this.board[this.row][this.col] = num;
     }
 
     @Override
     public boolean isGoal() {
+        return this.col == this.DIM - 1 && this.row == this.DIM - 1;
+    }
 
-        // TODO
+    public void preFill() {
+        preFilled = true;
+        //prefill
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < this.DIM; j++) {
+                if (this.lookingValues[i][j] == 1) {
+                    int r;
+                    int c;
+                    switch (i) {
+                        case 0 -> {
+                            r = 0;
+                            c = j;
+                        }
+                        case 1 -> {
+                            r = j;
+                            c = this.DIM - 1;
+                        }
+                        case 2 -> {
+                            r = this.DIM - 1;
+                            c = j;
+                        } default -> {
+                            r = j;
+                            c = 0;
+                        }
+                    }
+                    /**
+                     if (i == 0) {
+                        r = 0;
+                        c = j;
+                    } else if (i == 1) {
+                        r = j;
+                        c = this.DIM-1;
+                    } else if (i == 2) {
+                        r = this.DIM-1;
+                        c = j;
+                    } else {
+                        r = j;
+                        c = 0;
+                    }*/
+                    this.board[r][c] = 4;
+                    this.rows.get(r).add(4);
+                    this.columns.get(c).add(4);
+                }
+            }
+        }
 
-        return false; // remove after implementing
+        //top row
+        for (int i = 0; i < this.DIM; i++) {
+            if (this.lookingValues[0][i] == this.DIM) {
+                for (int j = 1; j <= this.DIM; j++) {
+                    this.board[j-1][i] = j;
+                    this.rows.get(j-1).add(j);
+                    this.columns.get(i).add(j);
+                }
+            }
+        }
+
+        //bottom row
+        for (int i = 0; i < this.DIM; i++) {
+            if (this.lookingValues[2][i] == this.DIM) {
+                int num = 1;
+                for (int j = 4; j >= 1; j--) {
+                    this.board[j-1][i] = num;
+                    this.rows.get(j-1).add(num);
+                    this.columns.get(i).add(num);
+                    num++;
+                }
+            }
+        }
+
+        //left row
+        for (int i = 0; i < this.DIM; i++) {
+            if (this.lookingValues[3][i] == this.DIM) {
+                for (int j = 1; j <= this.DIM; j++) {
+                    this.board[i][j-1] = j;
+                    this.rows.get(i).add(j);
+                    this.columns.get(j-1).add(j);
+                }
+            }
+        }
+
+        //right row
+        for (int i = 0; i < this.DIM; i++) {
+            if (this.lookingValues[1][i] == this.DIM) {
+                int num = 1;
+                for (int j = 4; j >= 1; j--) {
+                    this.board[i][j-1] = num;
+                    this.rows.get(i).add(num);
+                    this.columns.get(j-1).add(num);
+                    num++;
+                }
+            }
+        }
+    }
+
+    public Boolean quickFill() {
+        for (int i = 0; i < this.DIM; i++) {
+            if (this.rows.get(i).size() == this.DIM - 1) {
+                for (int j = 0; j < this.DIM; j++) {
+                    if (this.board[i][j] == EMPTY) {
+                        for (int k = 1; k <= this.DIM; k++) {
+                            if (!this.rows.get(i).contains(k)) {
+                                this.board[i][j] = k;
+                                this.rows.get(i).add(k);
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return this.isGoal();
     }
 
     /**
@@ -95,11 +228,34 @@ public class SkyscraperConfig implements Configuration {
      */
     @Override
     public Collection<Configuration> getSuccessors() {
+        if (!preFilled) {
+            this.preFill();
+        }
+        ArrayList<Configuration> successors = new ArrayList<>();
+        if (!this.quickFill()) {
 
-        // TODO
+            if (this.col == this.DIM - 1) {
+                this.col = 0;
+                this.row += 1;
+            } else {
+                this.col++;
+            }
 
-        return new ArrayList<>();   // remove after implementing
+            while (this.board[this.row][this.col] != EMPTY) {
+                if (this.col == this.DIM - 1) {
+                    this.col = 0;
+                    this.row += 1;
+                } else {
+                    this.col++;
+                }
+            }
+            for (int i = 1; i <= this.DIM; i++) {
+                SkyscraperConfig child = new SkyscraperConfig(this, this.row, this.col, i);
+                successors.add(child);
+            }
+        }
 
+        return successors;
     }
 
     /**
@@ -109,10 +265,18 @@ public class SkyscraperConfig implements Configuration {
      */
     @Override
     public boolean isValid() {
+        int current = this.board[this.row][this.col];
+        if (this.rows.get(this.row).contains(current) || this.columns.get(this.col).contains(current)) {
+            return false;
+        } else {
 
-        // TODO
+        }
 
-        return false;  // remove after implementing
+
+        this.rows.get(this.row).add(current);
+        this.columns.get(this.col).add(current);
+
+        return true;
     }
 
     /**
@@ -143,7 +307,11 @@ public class SkyscraperConfig implements Configuration {
         for (int i = 0; i < this.DIM; i++) {
             output.append(this.lookingValues[3][lookValue]).append("/ ");
             for (int j = 0; j < this.DIM; j++) {
-                output.append(this.board[i][j]).append(" ");
+                if (this.board[i][j] == EMPTY) {
+                    output.append(EMPTY_CELL).append(" ");
+                } else {
+                    output.append(this.board[i][j]).append(" ");
+                }
             }
             output.append("/").append(this.lookingValues[1][lookValue]).append("\n");
             lookValue++;
